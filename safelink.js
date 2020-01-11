d5d8972d8a080032e1004faa192ecf3f7b985ce8
prodesain <58811834+prodesain@users.noreply.github.com>
2020-01-11 22:00:47 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -26,7 +26,7 @@ if(x=="2"){xxx="data-science-and-its-rising-importance.html"}
 if(x=="3"){xxx="leukemia-treatment-breakthroughs-for.html"}
 if(x=="4"){xxx="5-ways-fukushima-radiation-releases-are.html"}
 if(x=="5"){xxx="5-benefits-of-marble-floors.html"}
-if(x=="6"){xxx="10-tips-to-help-you-repair-your-wooden.html"}a_to_ve[i].href="https://free-psd-link.blogspot.com/2019/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="6"){xxx="10-tips-to-help-you-repair-your-wooden.html"}a_to_ve[i].href="https://blog.sadesain.net/2019/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
